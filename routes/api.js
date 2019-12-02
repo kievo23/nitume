f26449157e60db7fcccf793f4c1f844d8f7ddaf9
kev <kelvinchege@gmail.com>
2019-12-03 00:25:14 +0300

    changes to api to accomodate service man

--- routes/api.js
+++ routes/api.js
@@ -262,7 +262,7 @@ router.post('/orderCompleted', async function(req, res, next) {
     if(user){
       let order = await Order.findOneAndRemove({user: user.id, _id: req.body.orderId});
       if(order){
-        order.status = req.body.status;
+        order.status = parseInt(req.body.status);
         let rst = await order.save();
         if(rst){
           res.json({code : 100, data: "The order status has been changed successfully"});
