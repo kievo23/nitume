a6e7269550ed1933b998857a00e4f1bd7e494cd0
kev <kelvinchege@gmail.com>
2019-12-03 00:13:41 +0300

    changes to api to accomodate service man

--- routes/api.js
+++ routes/api.js
@@ -254,6 +254,32 @@ router.post('/user/verifyfb',function(req, res){
   });
 });
 
+router.post('/orderCompleted', async function(req, res, next) {
+  if(req.body.phone){
+    var phone = req.body.phone.replace(/\s+/g, '');
+    phone = "254"+phone.substr(phone.length - 9);
+    let user = await User.findOne({phone: phone});
+    if(user){
+      let order = await Order.findOneAndRemove({user: user.id, _id: req.body.id});
+      if(order){
+        order.status = 2
+        let rst = await order.save();
+        if(rst){
+          res.json({code : 100, data: "The order status has been changed successfully"});
+        }else{
+          res.json({code : 101, data: "Oops! Order status wasnt changed. Try again later"});
+        }
+      }else{
+        res.json({code : 101, data: "Oops! Order doesn't exist"});
+      }
+    }else{
+      res.json({code : 101, data: "Oops! User doesn't exist"});
+    }    
+  }else{
+    res.json({code : 101, data: "Oops! no record found"});
+  }
+});
+
 /*
 router.post('/nearby', function(req, res){
   var point = { type : "Point", coordinates : [parseFloat(req.body.longitude),parseFloat(req.body.latitude)] };
